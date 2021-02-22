57efb322a2000c864df581900509fc34af030e58
Dima <budya2012@gmail.com>
2021-02-22 22:04:54 +0300

    feat: implement min-max-avg functions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-
 exports.min = function min (array) {
-  return 0;
-
+  if(array === undefined || array.length === 0) return 0;
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
-
+  if(array === undefined || array.length === 0) return 0;
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
-  
+  if(array === undefined || array.length === 0) return 0;
+  return array.reduce((current, item) => current + item, 0) / array.length;  
 }
